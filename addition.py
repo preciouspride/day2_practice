64b025826985c6600b4b9289541ae44be243c679
Precious Pride <prcslum@gmail.com>
2024-12-20 08:08:17 +0100

    comparism using if and else

--- addition.py
+++ addition.py
@@ -1,2 +1,8 @@
-print ("hello fam")
-print("hello Precious. How are you doing?")
+#print ("hello fam")
+#print("hello Precious. How are you doing?")
+students = [[42, 'Mofor', True, 178.23], [24, 'Kaba', True, 170.324], [32, 'Fru', True, 200.324]]
+kelly = [20, 'Kelly', False, 52.4]
+students.append(kelly)
+kelly_age =students [-1][0]
+print(kelly_age)
+       
---
+++ comparism.py
@@ -0,0 +1,13 @@
+kelly_age = 20
+kaba_age = 24
+mofor_age = 42
+Louise_age = 32 
+
+if kelly_age < kaba_age:
+        print("kaba is older") 
+elif mofor_age > kaba_age:
+        print("mofor is older than kaba")
+elif Louise_age >= mofor_age:
+        print("louise is older than or same age as mofor")
+else:
+        print("no one is older")
