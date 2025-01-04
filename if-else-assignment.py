c73febb371286660cb4f4ff87e06b5e80d902fe8
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2025-01-04 08:54:10 -0500

    assignment assigned

---
+++ if-else-assignment.py
@@ -0,0 +1,10 @@
+# TODO 1: Use input to collect a user's maths score over 100:
+# score = input("What is your grade: ")
+
+#TODO 2: Using the user's grade, find out the user's grade
+# if score > 95 => A+
+# if score > 90 => A
+# if score > 80 => B
+# if score > 70 => C
+# if score > 60 => D
+# if score < 60 => F
