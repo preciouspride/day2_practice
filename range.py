ac46aa377d1a94fbab7f5e84d96fa52a21795d0a
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2025-01-11 09:38:45 -0500

    add file to discuss range function

---
+++ range.py
@@ -0,0 +1,22 @@
+# #range() => function that creates  range object
+# #range takes 3 types of arguments - 1 argument, 2 arguments, and 3 arguments
+# var = range(5) # range object gives you variables from a starting number (inclusive) to an ending number (exclusive)
+# ## IF RANGE HAS 1 ARGUMENT, IT IS THE END, AND THE START IS 0
+# print(list(var))
+
+# var1 = range(0, 5) #IF RANGE TAKES 2 ARGUMENTS, THE FIRST IS THE STARTING ARGUMENT (INCLUSIVE), AND THE SECOND IS THE ENDING ARGUMENT (EXCLUSIVE)
+# print(list(var1))
+
+# var2 = range(3, 5)
+# print(list(var2))
+
+# var3 = range(0, 5, 1) #IF RANGE TAKES 3 ARGUMENTS, THE FIRST IS THE STARTING ARGUMENT (INCLUSIVE), AND THE SECOND IS THE ENDING ARGUMENT (EXCLUSIVE). THE 3 ARGUMENT IS THE STEP
+# print(list(var3))
+
+
+# var4 = range(0, 5, 2) #IF RANGE TAKES 3 ARGUMENTS, THE FIRST IS THE STARTING ARGUMENT (INCLUSIVE), AND THE SECOND IS THE ENDING ARGUMENT (EXCLUSIVE). THE 3 ARGUMENT IS THE STEP
+# print(list(var3))
+
+
+var4 = range(1, 11, 3) #IF RANGE TAKES 3 ARGUMENTS, THE FIRST IS THE STARTING ARGUMENT (INCLUSIVE), AND THE SECOND IS THE ENDING ARGUMENT (EXCLUSIVE). THE 3 ARGUMENT IS THE STEP
+print(list(var4))
