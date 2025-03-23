299ab1e6d59554f7bc2dd2509f40f87b70a46199
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2025-03-23 08:50:21 -0400

    classes definition

---
+++ classes.py
@@ -0,0 +1,52 @@
+## Classes 
+## OOP => Object Oriented Programming. Vs Functional Programming.
+## Object Oriented Programming works with classes.
+## A class is a blueprint/design for creating objects/types
+## Classes can be called, and can take an argument OR NOT
+## Some pillars of OOP include 
+##      1) Inheritance : Classes can have parents from which they inherit certain properties/attributes/methods
+##      2) Encapsulation : Classes can combine attributes and methods (behavior) together and only show how they can be used, but not how they are implemented
+##      3) Polymorphism : We can refer to a group of child classes using their parent class
+## Every class inherits from a parent call Object
+## Like functions, classes are callable => They have to be called in order to be used.
+## We create a class using the "class" keyword followed by the class name
+
+## An object has attributes which form the state
+
+## Classes have an __init__ method that is called to initialize an object that is created from this class 
+## Double underscore => dunder
+
+
+class Person:
+    '''This is called a DOCSTRING which is an documentation of a class'''
+
+    def __init__(self, fname, lname):
+        self.first_name = fname
+        self.last_name = lname
+
+kaba = Person("Emmanuel", "Kaba")
+kaba.age = 20
+kaba.height = 175
+kaba.weight = 70
+
+print("This is Kaba Person")
+print(kaba.first_name)
+print(kaba.last_name)
+print(kaba.age)
+print(kaba.height)
+print(kaba.weight)
+
+louis = Person("Louis", "Ndango")
+louis.age = 30
+louis.height = 175
+louis.weight = 185
+print("\n")
+print("This is Louis Person")
+print(louis.first_name)
+print(louis.last_name)
+print(louis.age)
+print(louis.weight)
+print(louis.height)
+
+print(type(louis))
+print(type(kaba))
